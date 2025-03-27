5c0ac8806ba28d92bc740214f2a5d33aa0ec4f04
John Stairs <jostairs@microsoft.com>
2025-03-27 20:08:45 +0000

    Add example

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # tyger-csharp-examples
+
+This repo contains an example of a container image that can function as a [Tyger](https://github.com/microsoft/tyger) run container. 
+
+First log into a Tyger instance:
+
+``` bash
+tyger login <URL|--local>
+```
+
+Then pipe some data into `tyger run exec` using the provided run spec:
+
+``` bash
+echo hello | tyger run exec -f run.yml
+```
+
+The provided implementation just echoes the input to the output, which the above
+command writes to standard out (and logs to standard error).
---
+++ run/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.CommandLine;
+
+var inputOption = new Option<string>("--input", "The input file") { IsRequired = true };
+inputOption.AddAlias("-i");
+
+var outputOption = new Option<string>("--output", "The output file") { IsRequired = true };
+outputOption.AddAlias("-o");
+
+var cmd = new RootCommand() { inputOption, outputOption };
+
+cmd.SetHandler((input, output) =>
+    {
+        using var inputStream = File.OpenRead(input);
+        using var outputStream = File.OpenWrite(output);
+
+        inputStream.CopyTo(outputStream);
+    },
+    inputOption, outputOption);
+
+return cmd.Invoke(args);
